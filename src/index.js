034b426108ecd18b3f93acc886db94b59cf1c421
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2019-07-04 00:07:40 +0800

    add cards

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import PokerCover from "./pokers/PokerCover";
+import PokerOne from "./pokers/PokerOne";
+import PokerTwo from "./pokers/PokerTwo";
+import PokerThree from "./pokers/PokerThree";
+import PokerFive from "./pokers/PokerFive";
+import PokerEight from "./pokers/PokerEight";
+import PokerThirteen from "./pokers/PokerThirteen";
+import PokerTwenty from "./pokers/PokerTwenty";
+import PokerForty from "./pokers/PokerForty";
+import PokerHundred from "./pokers/PokerHundred";
+import PokerInfinty from "./pokers/PokerInfinty";
+import PokerShaving from "./pokers/PokerShaving";
+import PokerCoffee from "./pokers/PokerCoffee";
+import PokerEat from "./pokers/PokerEat";
+import PokerDragons from "./pokers/PokerDragons";
+
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> scrum-poker-cards </h1>
+      <div className="card-deck">
+        {/* <PokerCover /> */}
+        <PokerOne />
+        <PokerTwo />
+        <PokerThree />
+        <PokerFive />
+        <PokerEight />
+        <PokerThirteen />
+        <PokerTwenty />
+        <PokerForty />
+        <PokerHundred />
+        <PokerInfinty />
+        <PokerShaving />
+        <PokerCoffee />
+        <PokerEat />
+        <PokerDragons />
+      </div>
+      <a
+        className="credit"
+        href="https://github.com/redbooth/scrum-poker-cards"
+      >
+        redbooth/scrum-poker-cards
+      </a>
     </div>
   );
 }
---
+++ src/pokers/PokerCoffee.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const PokerCoffee = () => {
+  return (
+    <svg
+      version="1.1"
+      id="Layer_1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 167.2 249.4"
+      enable-background="new 0 0 167.2 249.4"
+    >
+      <rect x="-5.7" y="-5.7" fill="#D07E6D" width="178.6" height="260.8" />
+      <g>
+        <g>
+          <circle fill="#BA3033" cx="83.6" cy="103.3" r="48.6" />
+          <path
+            fill="#E9C77C"
+            d="M122.6,132.3H44.7c6.4,8.6,15.5,14.9,26.1,17.8l0,0c0,0,5.2,1,5.2,7c0,0.2-1.8,33.8-1.8,33.8
+          c0,2.9,2.3,3.4,5.2,3.4h8.5c2.9,0,5.2-0.5,5.2-3.4l-2-33.8c-0.2-3.9,2.9-6.2,4.2-6.7C106.4,147.7,116,141.2,122.6,132.3z"
+          />
+        </g>
+        <circle fill="#FFFFFF" cx="99.9" cy="184" r="12.7" />
+      </g>
+    </svg>
+  );
+};
+
+export default PokerCoffee;
